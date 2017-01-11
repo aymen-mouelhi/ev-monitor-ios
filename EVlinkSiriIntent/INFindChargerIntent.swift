751c7aa1320a7f0829e4652c9505061f18a25a34
Aymen Mouelhi <aymen.mouelhi@gmail.com>
2017-01-11 23:39:09 +0100

    Create an Intent in Swift

--- EVlinkSiriIntent/INFindChargerIntent.swift
+++ EVlinkSiriIntent/INFindChargerIntent.swift
@@ -18,27 +18,16 @@ open class INFindChargerIntent : INIntent {
     var serviceName: String
     var sender: INPerson?
     
-    public init(recipients: [INPerson]?, content: String?, groupName: String?, serviceName: String?, sender: INPerson?){
-        self.recipients = recipients
-        self.content = content
-        self.groupName = groupName
-        self.serviceName = serviceName
-        self.sender = sender
-    }
-    
-    required convenience init?(coder aDecoder: NSCoder) {
-        self.init(
-            recipients: recipients,
-            content: content,
-            groupName: groupName,
-            serviceName: serviceName,
-            sender: sender
-        );
-    }
-    
-    
+    /*
     // Contacts to whom the message should be sent.
-    open var recipients: [INPerson]? { return self.recipients }
+    open var recipients: [INPerson]? {
+        get {
+            return self.recipients
+        }
+        set (recip){
+            
+        }
+    }
     
     
     // Body text of the message.
@@ -54,5 +43,25 @@ open class INFindChargerIntent : INIntent {
     
     // The person, or account, sending the message.
     @NSCopying open var sender: INPerson? { return self.sender }
+    */
+    
+    public init(recipients: [INPerson]?, content: String?, groupName: String?, serviceName: String?, sender: INPerson?){
+        self.recipients = recipients!
+        self.content = content!
+        self.groupName = groupName!
+        self.serviceName = serviceName!
+        self.sender = sender
+    }
+    
+    required convenience public init?(coder aDecoder: NSCoder) {
+        self.init(
+            recipients: self.recipients,
+            content: self.content,
+            groupName: self.groupName,
+            serviceName: self.serviceName,
+            sender: self.sender
+        );
+    }
+    
 }
 
